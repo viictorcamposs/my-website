ff58cdc7c1c6d86b7c671c2d30f41c367506d488
Victor Campos <campos.viictor@gmail.com>
2023-05-03 09:40:15 -0300

    feat: create the projects API route to understand how it works inside the Next appDir

---
+++ src/app/(routes)/projects/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const projects = await new Promise((resolve, _) => {
+    const data = [
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      },
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      },
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      },
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      },
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      },
+      {
+        title: 'Nullam lobortis ullamcorper',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus mattis iaculis.',
+        imageUrl:
+          'https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      }
+    ]
+
+    setTimeout(() => resolve(data), 10000)
+  })
+
+  return NextResponse.json({ projects })
+}
