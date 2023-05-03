77d4654c9c64a69b2b95cb00b2afe3a3656c2e16
Victor Campos <campos.viictor@gmail.com>
2023-05-03 09:35:45 -0300

    feat: create main page with mobile design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <span className="hidden">Hello World</span>
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="font-heading text-4xl min-[375px]:text-[42px] font-extrabold text-[#0C0F17]">
+        Victor Campos
+      </h1>
+
+      <h2 className="text-base font-body mt-1.5 text-[#464444]">
+        SOFTWARE ENGINEER <span>🇧🇷</span>
+      </h2>
+    </main>
+  )
 }
