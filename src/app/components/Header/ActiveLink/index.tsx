e07f08e38cb6f8c38a7ac01e1cbe0a2db70c8e43
Victor Campos <campos.viictor@gmail.com>
2023-06-01 17:54:22 -0300

    refactor: use the next.js docs suggested way to style active link based on the route

--- src/app/components/Header/ActiveLink/index.tsx
+++ src/app/components/Header/ActiveLink/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import type { LinkProps } from 'next/link'
 import Link from 'next/link'
@@ -20,32 +19,21 @@ const linkClassName = `
   after:block after:absolute
   after:bottom-2 after:left-[calc(50%-10px)]
   after:h-[2px] after:w-[20px]
-  after:bg-transparent
   after:transition-all after:duration-200
 `
 
 export default function ActiveLink({ children, ...props }: IActiveLink) {
-  const [computedClassName, setComputedClassName] = useState(linkClassName)
-
   const pathname = usePathname()
 
-  useEffect(() => {
-    const linkPathname = new URL(props.href as string, window.location.href).pathname
-
-    const activeLinkClassName = linkClassName.replace(
-      'after:bg-transparent',
-      'after:bg-[#0c0f17] dark:after:bg-[#f7f5f9]'
-    )
-
-    if (pathname.includes(linkPathname)) {
-      setComputedClassName(activeLinkClassName)
-    } else {
-      setComputedClassName(linkClassName)
-    }
-  }, [props.href, pathname])
-
   return (
-    <Link className={computedClassName} {...props}>
+    <Link
+      {...props}
+      className={
+        pathname.startsWith(props.href as string)
+          ? `${linkClassName} after:bg-[#0c0f17] dark:after:bg-[#f7f5f9]`
+          : `${linkClassName} after:bg-transparent`
+      }
+    >
       {children}
     </Link>
   )
