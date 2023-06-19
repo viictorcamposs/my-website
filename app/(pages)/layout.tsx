b53fecbf8ee9b89c2c9c541b7f7180957ad3fc04
Victor Campos <campos.viictor@gmail.com>
2023-06-19 17:06:40 -0300

    refactor: remove fonts from the layout file and import from fonts.ts

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,25 +1,11 @@
-import { Poppins, Inter } from 'next/font/google'
-
 import Header from '~/app/components/Header'
 import Footer from '~/app/components/Footer'
+
+import { inter, poppins } from '../fonts'
 
 import './globals.css'
 
 import 'server-only'
-
-const poppins = Poppins({
-  weight: ['400', '500', '600', '700', '800'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins'
-})
-
-const inter = Inter({
-  weight: ['300', '400', '500', '600', '700', '800'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter'
-})
 
 export const metadata = {
   title: 'Victor Campos | Page',
---
+++ app/fonts.ts
@@ -0,0 +1,15 @@
+import { Poppins, Inter } from 'next/font/google'
+
+export const poppins = Poppins({
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins'
+})
+
+export const inter = Inter({
+  weight: ['300', '400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter'
+})
