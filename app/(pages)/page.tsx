280b60b883ae46ec53d5b17b4554a52bc096c25b
Victor Campos <campos.viictor@gmail.com>
2023-06-29 19:40:53 -0300

    feat: create new ui for the home page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,14 +1,27 @@
-export default function Home() {
-  return (
-    <main className="flex flex-col justify-center items-center">
-      <h1 className="font-heading text-4xl sm:text-[42px]/[1.5] xl:text-5xl/[1.5] font-extrabold text-[#f7f5f9]">
-        Victor Campos
-      </h1>
+import { compareDesc } from 'date-fns'
+import type { Article } from 'contentlayer/generated'
+import { allArticles } from 'contentlayer/generated'
 
-      <h2 className="text-base xl:text-xl font-body mt-1.5 text-[#c7c8e0]">
-        SOFTWARE ENGINEER
-        <span className="sm:text-lg xl:text-3xl ml-2 xl:relative xl:top-1">🇧🇷</span>
-      </h2>
-    </main>
+import ListArticles from '../components/ListArticles'
+import Header from '../components/Header'
+
+const main = `
+  w-full max-w-[680px] mx-auto 
+  px-5 min-[375px]:px-7 md:px-0
+`
+
+export default function Home() {
+  const articles: Article[] = allArticles.sort((a, b) =>
+    compareDesc(new Date(a.releaseDate), new Date(b.releaseDate))
+  )
+
+  return (
+    <>
+      <Header />
+
+      <main className={main}>
+        <ListArticles articles={articles} />
+      </main>
+    </>
   )
 }
