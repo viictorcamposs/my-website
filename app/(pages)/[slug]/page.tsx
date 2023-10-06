972f6ac70de7ec53dfefbc877a0940c2fa060669
Victor Campos <campos.viictor@gmail.com>
2023-10-06 10:49:59 -0300

    refactor: change implementation of MDX in the /[slug] page

--- app/(pages)/[slug]/page.tsx
+++ app/(pages)/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from 'next/navigation'
 import type { Metadata } from 'next'
 
-import { getMDXComponent } from 'next-contentlayer/hooks'
+import { useMDXComponent } from 'next-contentlayer/hooks'
 import { allPosts } from 'contentlayer/generated'
 
 import { components } from '~/app/components/MdxComponents'
@@ -64,7 +64,7 @@ export default function Page({ params: { slug } }: IProps) {
 
   const { title, body, headings, hero } = getPartialPost(post)
 
-  const MDXContent = getMDXComponent(body.code)
+  const MDXContent = useMDXComponent(body.code)
 
   return (
     <>
