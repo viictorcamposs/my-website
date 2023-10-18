f94b55013f3ca97404917ec0e2844b388bfa4924
Victor Campos <campos.viictor@gmail.com>
2023-10-17 21:14:30 -0300

    Revert "refactor: improve readability of /og route.tsx file"
    This reverts commit 6af22e28e4216b2611a26ffda66246913a6e77e8.

--- app/(api)/og/route.tsx
+++ app/(api)/og/route.tsx
@@ -4,26 +4,15 @@ import { ImageResponse } from 'next/server'
 
 export const runtime = 'edge'
 
-const getImage = async () => {
-  const response = await fetch(new URL('../../assets/preview.png', import.meta.url))
-
-  const image = await response.arrayBuffer()
-
-  return new TextDecoder().decode(image)
-}
-
-const getFont = async () => {
-  const response = await fetch(new URL('../../assets/Poppins-Bold.ttf', import.meta.url))
-
-  return await response.arrayBuffer()
-}
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
 
   const title = searchParams.get('title')!
 
-  const [image, poppins] = await Promise.all([getImage(), getFont()])
+  const [image, poppins] = await Promise.all([
+    fetch(new URL('../../assets/preview.png', import.meta.url)).then(res => res.arrayBuffer()),
+    fetch(new URL('../../assets/Poppins-Bold.ttf', import.meta.url)).then(res => res.arrayBuffer())
+  ])
 
   let fontSize: string
 
@@ -36,7 +25,7 @@ export async function GET(request: Request) {
   return new ImageResponse(
     (
       <div tw="relative flex">
-        <img src={image} width={844} height={440} />
+        <img src={image as unknown as string} width={844} height={440} />
 
         <div tw="absolute w-full h-full flex items-center justify-center">
           <span
